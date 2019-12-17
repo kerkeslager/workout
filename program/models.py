956b7267f0ab9acfd37ef9d051792ad4fb700d42
David Kerkeslager <kerkeslager@gmail.com>
2019-12-17 02:43:52 -0500

    Some model updates

--- program/models.py
+++ program/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Program(models.Model):
     identifier = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, null=False, blank=False)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Program(models.Model):
 class ProgramWorkout(models.Model):
     identifier = models.UUIDField(default=uuid.uuid4, editable=False)
     program = models.ForeignKey('Program', models.CASCADE, related_name='workouts')
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, null=False, blank=False)
     exercises = models.ManyToManyField('exercise.Exercise', through='ProgramExercise')
 
     def __str__(self):
@@ -21,9 +21,9 @@ class ProgramWorkout(models.Model):
 class ProgramExercise(models.Model):
     workout = models.ForeignKey('ProgramWorkout', models.CASCADE)
     exercise = models.ForeignKey('exercise.Exercise', models.CASCADE)
-    weight = models.IntegerField()
-    sets = models.IntegerField()
-    reps = models.IntegerField()
+    weight = models.IntegerField(null=False)
+    sets = models.IntegerField(null=False)
+    reps = models.IntegerField(null=False)
 
     def __str__(self):
         return self.exercise.name
--- user/models.py
+++ user/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    programs = models.ManyToManyField('program.Program')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -17,3 +18,14 @@ def save_user_profile(sender, instance, **kwargs):
 
 class WorkoutRecord(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workout_records')
+    complete = models.BooleanField(null=False, default=False)
+
+class ExerciseRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='exercise_records')
+    workout_record = models.ForeignKey(WorkoutRecord, on_delete=models.CASCADE)
+    exercise = models.ForeignKey('exercise.Exercise', on_delete=models.PROTECT)
+
+class SetRecord(models.Model):
+    exercise_record = models.ForeignKey(ExerciseRecord, on_delete=models.CASCADE, related_name='set_records')
+    planned_reps = models.IntegerField(null=False)
+    completed_reps = models.IntegerField(null=True, default=None)
